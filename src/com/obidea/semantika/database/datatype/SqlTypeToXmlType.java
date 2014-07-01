ca3c36cd34953afc64b2653fa00c947c30b05080
Josef Hardi <josef.hardi@gmail.com>
2014-07-01 10:06:17 +0700

    Make SQL OTHER type equivalent to XSD STRING type.

--- src/com/obidea/semantika/database/datatype/SqlTypeToXmlType.java
+++ src/com/obidea/semantika/database/datatype/SqlTypeToXmlType.java
@@ -54,7 +54,7 @@ public final class SqlTypeToXmlType
       mTypeMapping.put(Types.TIMESTAMP, DataType.DATE_TIME);
       mTypeMapping.put(Types.BOOLEAN, DataType.BOOLEAN);
       mTypeMapping.put(Types.BIT, DataType.BOOLEAN);
-      mTypeMapping.put(Types.OTHER, DataType.PLAIN_LITERAL);
+      mTypeMapping.put(Types.OTHER, DataType.STRING);
    }
 
    /**
