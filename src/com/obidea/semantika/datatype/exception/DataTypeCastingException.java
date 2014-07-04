7f339cf283329029f66514d5a4d1442018676595
Josef Hardi <josef.hardi@gmail.com>
2014-07-04 10:16:16 +0700

    Move exception to datatype.exception package.

--- src/com/obidea/semantika/datatype/exception/DataTypeCastingException.java
+++ src/com/obidea/semantika/datatype/exception/DataTypeCastingException.java
@@ -13,9 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.obidea.semantika.datatype;
+package com.obidea.semantika.datatype.exception;
 
-import com.obidea.semantika.datatype.exception.DataTypeReasonerException;
 
 public class DataTypeCastingException extends DataTypeReasonerException
 {
